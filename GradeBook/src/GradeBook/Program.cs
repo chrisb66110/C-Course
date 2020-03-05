7591c9568d5cd97a62645fe1af493d1e9daaa3f2
christofer <chrisb66110@gmail.com>
2020-03-04 21:23:26 -0600

    Use List

--- GradeBook/src/GradeBook/Program.cs
+++ GradeBook/src/GradeBook/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GradeBook
 {
@@ -9,7 +10,7 @@ namespace GradeBook
             var name = args.Length > 0 ? args[0] : "World";
             Console.WriteLine($"Hello {name}!");
 
-            var numbers = new double[] {2.14, 1.14, 0.14, 1};
+            var numbers = new List<double>() {2.14, 1.14, 0.14, 1};
             
             var result = 0D;
             foreach(var number in numbers)
