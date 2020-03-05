e22eeda11f01b226e000a4734d1cb367f3fefe53
christofer <chrisb66110@gmail.com>
2020-03-04 20:59:43 -0600

    Adding numbers

--- GradeBook/src/GradeBook/Program.cs
+++ GradeBook/src/GradeBook/Program.cs
@@ -8,6 +8,11 @@ namespace GradeBook
         {
             var name = args.Length > 0 ? args[0] : "World";
             Console.WriteLine($"Hello {name}!");
+
+            var number1 = 2.14;
+            var number2 = 1;
+            var result = number1 + number2;
+            System.Console.WriteLine(result);
         }
     }
 }
