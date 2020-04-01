d0bd872316831dd64a720bb5e331977dc90cb513
christofer <chrisb66110@gmail.com>
2020-03-31 22:10:20 -0600

    Add TypeTest, testing differnet between reference and basic types

---
+++ GradeBook/test/GradeBook.Tests/TypeTest.cs
@@ -0,0 +1,47 @@
+using System;
+using Xunit;
+
+namespace GradeBook.Tests
+{
+    public class TypeTest
+    {
+        [Fact]
+        public void DifferentReferenceTest()
+        {
+            var book01 = GivenTheDefaultBook("Book01");
+            var book02 = GivenTheDefaultBook("Book02");
+         
+            Assert.Equal("Book01", book01.Name);
+            Assert.Equal("Book02", book02.Name);
+            Assert.NotSame(book01, book02);
+            Assert.NotEqual(book01, book02);
+        }
+        
+        [Fact]
+        public void SameReferenceTest()
+        {
+            var book01 = GivenTheDefaultBook("Book01");
+            var book02 = book01;
+         
+            Assert.Equal("Book01", book01.Name);
+            Assert.Equal("Book01", book02.Name);
+            Assert.Same(book01, book02);
+            Assert.Equal(book01, book02);
+        }
+
+        public Book GivenTheDefaultBook(string nameBook){
+            var book = new Book(nameBook);
+            book.AddGrade(10);
+            book.AddGrade(20);
+            book.AddGrade(30);
+            book.AddGrade(40);
+            book.AddGrade(50);
+            book.AddGrade(60);
+            book.AddGrade(70);
+            book.AddGrade(80);
+            book.AddGrade(90);
+
+            return book;
+        }
+    }
+}
