af4f28f7f87b376a78b1af586d760ab3d015269f
christofer <chrisb66110@gmail.com>
2020-04-03 23:53:30 -0600

    Add StatisticsTest

---
+++ GradeBook/test/GradeBook.Tests/StatisticsTest.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Xunit;
+
+namespace GradeBook.Tests
+{
+    [ExcludeFromCodeCoverage]
+    public class StatisticsTest
+    {
+        [Fact]
+        public void GetStatisticsTest()
+        {
+            var statistics = GivenTheDefaultStatistics();
+
+            statistics.ShowPreStatistics();
+            statistics.ShowStatistics();
+        }
+
+        public Statistics GivenTheDefaultStatistics()
+        {
+            double minValue = 10;
+            double avgValue = 50;
+            double maxValue = 100;
+            double minPreValue = 10;
+            double avgPreValue = 50; 
+            double maxPreValue = 100;
+            var response = new Statistics(minValue, avgValue, maxValue, minPreValue, avgPreValue, maxPreValue);
+            return response;
+        }
+    }
+}
